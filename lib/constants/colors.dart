b14114f947e9d66beb192cba69800d831fbbcac7
shaileshhb <shaileshb.0720@gmail.com>
2022-07-23 21:02:26 +0530

    initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color redColor = Color(0xFFDA4040);
+const Color blueColor = Color(0xFF5F52EE);
+
+const Color blackColor = Color(0xFF3A3A3A);
+const Color greyColor = Color(0xFF717171);
+
+const Color bgColor = Color(0xFFEEEFF5);
---
+++ lib/screens/home.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: buildAppBar(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: const Text(
+                      "All todo_flutters",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(Icons.search, color: Colors.black, size: 20),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: "Search",
+          hintStyle: TextStyle(color: greyColor),
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: bgColor,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        const Icon(
+          Icons.menu,
+          color: blackColor,
+          size: 30,
+        ),
+        SizedBox(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.png'),
+            )),
+      ]),
+    );
+  }
+}
